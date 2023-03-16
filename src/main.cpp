af1706896467bb67d542677f417ddc0c5bc962d7
DeathofBrain <l1015178747@outlook.com>
2023-03-16 19:17:34 +0800

    building main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <thread>
 #include <opencv2/opencv.hpp>
 #include "../lib/SerialCOM/SerialCom.h"
 #include "../lib/DataStruct/Structs.hpp"
@@ -13,34 +14,58 @@ double SerialPort::pitch_now = 0.0;
 double SerialPort::yaw_now = 0.0;
 
 // def functions
-void PortThread();//串口通信
-void UDPThread();//UDP通信
-
+void PortThread(); // 串口通信
+void UDPThread();  // UDP通信
 
 int main(int argc, char const *argv[])
 {
-    //参数提取颜色信息
+    // 参数提取颜色信息
     int color;
+    EnemyColor enemyColor;
     if (argc == 1)
     {
-        cout<<"请输入敌方装甲板颜色，0 == 红，1 == 蓝"<<endl;
-        cin>>color;
-    }
-    else if (argc == 2)
-    {
-        if (int i = std::stoi(argv[1]);i == 0||i == 1)
+        cout << "请输入敌方装甲板颜色 0 == 红 1 == 蓝" << endl;
+        cin >> color;
+        if (color == 0 || color == 1)
         {
-            cout<<"敌方装甲板颜色代号为"<<i<<endl;
-            color = i;
+            enemyColor = (EnemyColor)color;
         }
         else
         {
-            cout<<"非法参数"<<endl;
+            cout << "非法参数" << endl;
             return 1;
         }
     }
-    
-    
+    else if (argc == 2)
+    {
+        if (int i = std::stoi(argv[1]); i == 0 || i == 1)
+        {
+            cout << "敌方装甲板颜色代号为" << i << endl;
+            color = i;
+            enemyColor = (EnemyColor)color;
+        }
+        else
+        {
+            cout << "非法参数" << endl;
+            return 1;
+        }
+    }
+    // 检测视频流
+    int videoflag = 0;
+    VideoCapture cap; // 读取视频流
+    if (videoflag == 1)
+    {
+        cap.open("/home/neuqrm/NOROS/top.avi");
+    }
+    // 相机相关文件路径
+    //const string camera_config_path = HIK_CONFIG_FILE_PATH"/camera_config.yaml";             // 相机配置文件路径
+    //const string intrinsic_para_path = HIK_CALI_FILE_PATH"/caliResults/calibCameraData.yml"; // 相机内参文件路径
+    //相机初始化
+    //TODO
+    //启动串口通信
+    thread(PortThread).detach();
+    //启动UDP通信
+    //thread(UDPThread).detach();//TODO
     
 }
 
